2e6e3685762c838e3a0a00e3dd1f17e396870163
Gumeniuk Ruslan <bishob1997@gmail.com>
2023-04-01 12:26:42 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle"
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const playerLastTime = localStorage.getItem('videoplayer-current-time');
+const setPlayerTime = data => localStorage.setItem('videoplayer-current-time', data.seconds);;
+
+if (playerLastTime) {
+  player.setCurrentTime(playerLastTime)
+}
+
+player.on('timeupdate', throttle(setPlayerTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from "lodash.throttle"
 
+const form = document.querySelector('.feedback-form');
+const formData = {};
+
+form.addEventListener('submit', submitForm)
+form.addEventListener('input', throttle(userInputData, 500))
+
+
+function submitForm(evt) {
+  evt.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  evt.currentTarget.reset();
+}
+
+function userInputData(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
